d9a083edb9b66e1e8a90d3ef893b0d8ddd607aac
Josh Mainini <joshmainini@outlook.com>
2017-06-27 11:04:49 -0400

    clean up

--- M3W2D1-controllers-part1-exercises/Exercises.Web/Controllers/HomeController.cs
+++ M3W2D1-controllers-part1-exercises/Exercises.Web/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Exercises.Web.Models;
 
 namespace Exercises.Web.Controllers
 {
@@ -13,5 +14,21 @@ namespace Exercises.Web.Controllers
         {
             return View();
         }
+		public ActionResult OrderName()
+		{
+			return View("OrderName");
+		}
+		public ActionResult OrderNameResult(OrderNameModel model)
+		{
+			return View("OrderNameResult", model);
+		}
+		public ActionResult ColorizedName()
+		{
+			return View("ColorizedName");
+		}
+		public ActionResult ColorizedNameResult(ColorizedNameModel colorizedModel)
+		{
+			return View("ColorizedNameResult",colorizedModel);
+		}
     }
 }
---
+++ M3W2D1-controllers-part1-exercises/Exercises.Web/Models/ColorizedNameModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Exercises.Web.Models
+{
+	public class ColorizedNameModel
+	{
+		public string FirstName {get; set;}
+		public string LastName { get; set; }
+		public string Green { get; set; }
+		public string Blue { get; set; }
+		public string Red { get; set; }
+	}
+}
