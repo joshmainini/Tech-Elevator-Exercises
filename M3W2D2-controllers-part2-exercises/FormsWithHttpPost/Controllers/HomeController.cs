60dabeae1488a4f87296443f4ad0e96ede5825e1
Josh Mainini <joshmainini@outlook.com>
2017-06-28 20:15:20 -0400

    Completed controllers part 2 exercises

--- M3W2D2-controllers-part2-exercises/FormsWithHttpPost/Controllers/HomeController.cs
+++ M3W2D2-controllers-part2-exercises/FormsWithHttpPost/Controllers/HomeController.cs
@@ -5,17 +5,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Configuration;
+using FormsWithHttpPost.Models;
+using FormsWithHttpPost.Models;
 
 namespace FormsWithHttpPost.Controllers
 {
     public class HomeController : Controller
     {
-        
+		string connectionString = WebConfigurationManager.ConnectionStrings["ProductReview"].ToString();
 
-        // GET: Home
-        public ActionResult Index()
+		// GET: Home
+		public ActionResult Index()
         {
-            return View();
-        }        
+            return View("Index");
+        }
+		public ActionResult NextReview()
+		{
+			return View("NextReview");
+		}
+		[HttpPost]
+		public ActionResult NextReview(Review review)
+		{
+			ReviewSqlDAL dal = new ReviewSqlDAL(connectionString);
+			dal.SaveReview(review);
+			return RedirectToAction("Index");
+		}
     }
 }
--- M3W2D2-controllers-part2-exercises/FormsWithHttpPost/DAL/ReviewSqlDAL.cs
+++ M3W2D2-controllers-part2-exercises/FormsWithHttpPost/DAL/ReviewSqlDAL.cs
@@ -4,21 +4,76 @@ using System.Linq;
 using System.Web;
 using FormsWithHttpPost.Models;
 using System.Data.SqlClient;
+using FormsWithHttpPost.Models;
 
 namespace FormsWithHttpPost.DAL
 {
     public class ReviewSqlDAL : IReviewDAL
     {
+		private string connectionString;
 
+		public ReviewSqlDAL(string connectionString)
+		{
+			this.connectionString = connectionString;
+		}
 
         public List<Review> GetAllReviews()
         {
-            throw new NotImplementedException();
-        }
+			List<Review> output = new List<Review>();
+
+			try
+			{
+				using (SqlConnection connection = new SqlConnection(connectionString))
+				{
+					connection.Open();
+					SqlCommand cmd = new SqlCommand("SELECT * FROM reviews", connection);
+					SqlDataReader reader = cmd.ExecuteReader();
+
+					while (reader.Read())
+					{
+						Review r = new Review();
+						r.Id = Convert.ToInt32(reader["review_id"]);
+						r.Username = Convert.ToString(reader["username"]);
+						r.Rating = Convert.ToInt32(reader["rating"]);
+						r.Title = Convert.ToString(reader["review_title"]);
+						r.Message = Convert.ToString(reader["review_text"]);
+						r.ReviewDate = Convert.ToDateTime(reader["review_date"]);
+						output.Add(r);
+					}
+				}
+			}
+			catch (SqlException e)
+			{
+				throw;
+			}
+			return output;
+		}
 
         public bool SaveReview(Review newReview)
         {
-            throw new NotImplementedException();
-        }
+			try
+			{
+				using (SqlConnection conn = new SqlConnection(connectionString))
+				{
+					conn.Open();
+
+					SqlCommand cmd = new SqlCommand("INSERT INTO reviews VALUES(@userName, @rating, @reviewTitle, @reviewText, @review_Date)", conn);
+					cmd.Parameters.AddWithValue("@username", newReview.Username);
+					cmd.Parameters.AddWithValue("@rating", newReview.Rating);
+					cmd.Parameters.AddWithValue("@reviewTitle", newReview.Title);
+					cmd.Parameters.AddWithValue("@reviewText", newReview.Message);
+					cmd.Parameters.AddWithValue("@review_Date", DateTime.UtcNow);
+
+					int rowsAffected = cmd.ExecuteNonQuery();
+
+					return rowsAffected > 0;
+				}
+			}
+			catch (SqlException ex)
+			{
+				// Error Logging that a problem occurred, don't show the user
+				throw;
+			}
+		}
     }
 }
