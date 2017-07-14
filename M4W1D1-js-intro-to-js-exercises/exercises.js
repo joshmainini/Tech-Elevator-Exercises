cf326e07c86f5646411603ab4086be47f7dab316
John Fulton <john@TechElevator.com>
2017-07-14 08:40:12 -0400

    M4W1D5-twitter-bootstrap-exercises

--- M4W1D1-js-intro-to-js-exercises/exercises.js
+++ M4W1D1-js-intro-to-js-exercises/exercises.js
@@ -1,1 +1,10 @@
-﻿
+﻿function countValues(input) {
+    var count = {};
+    for (var i = 0; i < input.length; i++) {
+        if (isNaN(count[input[i]])) {
+            count[input[i]] = 0;
+        }
+        count[input[i]] += 1;
+    }
+    return count;
+}
